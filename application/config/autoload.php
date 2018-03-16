cda0f00f1830c0e21d7d0b793b5e2909dba9dbf9
Shery <sheryarahmed007@gmail.com>
2018-03-17 04:14:21 +0500

    Uploading basic structure of project with minimal features

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'html', 'form', 'typography', 'security', 'pd_helper', 'cookie');
+$autoload['helper'] = array('url', 'html', 'form', 'typography', 'security', 'es_helper', 'cookie');
 
 /*
 | -------------------------------------------------------------------
